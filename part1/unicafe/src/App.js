e84cdd6f6806af4f06dd8d1a5b35008ed5d3755b
cata <gchelaru63@gmail.com>
2023-02-28 11:36:35 +0200

    finished part1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>props.name</h1>
+    </div>
+  )
+}
+
+const Feedback = ({text,value}) => {
+  if (text === "positive") {
+    return (
+      <tr><td>{text} {value} %</td></tr>
+    )
+  }
+
+  return (
+    <tr><td>{text} {value}</td></tr>
+  )
+}
+
+const Statistics = ({clicks}) => {
+  const total = clicks.good + clicks.neutral + clicks.bad
+  const average = (clicks.good * 1 + clicks.bad * -1) / total
+  const positive = clicks.good * (100/total)
+
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Feedback text="good" value={clicks.good} />
+          <Feedback text="neutral" value={clicks.neutral} />
+          <Feedback text="bad" value={clicks.bad} />
+          <Feedback text="all" value={total} />
+          <Feedback text="average" value={average} />
+          <Feedback text="positive" value={positive} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const handleGoodClick = () =>
+    setClicks({...clicks, good: clicks.good + 1})
+
+  const handleNeutralClick = () =>
+    setClicks({...clicks, neutral: clicks.neutral + 1})
+
+  const handleBadClick = () =>
+    setClicks({...clicks, bad: clicks.bad + 1})
+
+
+  return (
+    <div>
+      <Header name="Customer feedback" />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Header name="Statistics" />
+      <Statistics clicks={clicks} />
+    </div>
+  )
+}
+
+
+export default App
